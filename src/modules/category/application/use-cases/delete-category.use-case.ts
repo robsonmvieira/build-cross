6e34c74b5c3d5440c1a9e7a7953dae7f9ee5030a
Robson Maia <robsonmvieira@gmail.com>
2022-06-22 13:28:13 -0300

    feat(modules): :sparkles: add deleteCategoryUseCase

---
+++ src/modules/category/application/use-cases/delete-category.use-case.ts
@@ -0,0 +1,21 @@
+import {  DeleteCategoryDTO } from "../../dtos";
+import CategoryRepository  from "../../infra/repository/i-category.repository";
+import { UseCase as DefaultUseCase } from '../../../../core/application/use-cases'
+
+
+export namespace DeleteCategoryUseCase {
+  
+  export class UseCase implements DefaultUseCase<DeleteCategoryDTO, void> {
+    constructor(
+      private readonly categoryRepository: CategoryRepository.ICategoryRepository,
+    ) {}
+  
+    async execute(input: DeleteCategoryDTO): Promise<void>{
+      await this.categoryRepository.delete(input.id);
+    }
+  }
+
+}
+export default DeleteCategoryUseCase
+
+
